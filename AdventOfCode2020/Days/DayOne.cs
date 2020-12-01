9dbcd0dcf43377986136b4d171ffbf623fc4eb56
Stéphane FRACHE <stephane.frache@keyconsulting.fr>
2020-12-01 17:40:33 +0100

    Changed indent

--- AdventOfCode2020/Days/DayOne.cs
+++ AdventOfCode2020/Days/DayOne.cs
@@ -15,7 +15,8 @@ namespace AdventOfCode2020.Days
             Console.WriteLine("Day one - Part One");
             List<int> lines = PuzzleInputHelper.GetInputLines("DayOne.txt").Select(l => int.Parse(l)).ToList();
             Console.WriteLine($"Input has {lines.Count} lines");
-            int prod = lines.SelectMany(a => lines.Select(b => (a+b, a*b))).Where(sum => sum.Item1 == 2020).First().Item2;
+            int prod = lines.SelectMany(a => lines.Select(b => (a+b, a*b)))
+                            .Where(sum => sum.Item1 == 2020).First().Item2;
             Console.WriteLine($"Product of lines with sum 2020 is {prod}");
             Console.WriteLine("Day one - End of part One");
         }
@@ -26,7 +27,8 @@ namespace AdventOfCode2020.Days
             List<int> lines = PuzzleInputHelper.GetInputLines("DayOne.txt").Select(l => int.Parse(l)).ToList();
             Console.WriteLine($"Input has {lines.Count} lines");
             int prod = lines.SelectMany(a => lines.Select(b => (a + b, a * b)))
-                        .SelectMany(ab => lines.Select(c => (ab.Item1 + c, ab.Item2 * c))).Where(sum => sum.Item1 == 2020).First().Item2;
+                            .SelectMany(ab => lines.Select(c => (ab.Item1 + c, ab.Item2 * c)))
+                            .Where(sum => sum.Item1 == 2020).First().Item2;
             Console.WriteLine($"Product of lines with sum 2020 is {prod}");
             Console.WriteLine("Day one - End of part Two");
         }
