80209a6ab83cc1f541b1fca44ccae05f9fd97101
Stéphane FRACHE <stephane.frache@keyconsulting.fr>
2024-12-24 16:47:57 +0100

    Updated part 2 tests

--- AdventOfCode2024.Tests/Days/Day06Tests.cs
+++ AdventOfCode2024.Tests/Days/Day06Tests.cs
@@ -31,66 +31,18 @@ public class Day06Tests
     }
     
     [Test]
-    public void Part2_Should_Output_Number_Of_Places_Visited_By_Guard_When_He_Has_To_Turn_Right_Once()
+    public void Part2_Should_Output_One_Place_That_Would_Create_A_Loop_When_There_Is_Only_One()
     {
         char[][] fakeInput =
         [
-            "#....".ToArray(),
-            ".....".ToArray(),
-            "^....".ToArray(),
-            ".....".ToArray(),
-            ".....".ToArray()
-        ];
-        
-        string expectedMessage = $"Number of different visited places: 6";
-        Mock<IPuzzleInputHelper> puzzleInputHelperMock = new Mock<IPuzzleInputHelper>();
-        puzzleInputHelperMock.Setup(p => p.GetInputMatrix("Day06.txt")).Returns(fakeInput);
-        Day06 day06 = new Day06(puzzleInputHelperMock.Object);
-        Mock<TextWriter> tw = new Mock<TextWriter>();
-        Console.SetOut(tw.Object);
-        
-        day06.Part1();
-        
-        tw.Verify(t => t.WriteLine(expectedMessage), Times.Once);
-    }
-    
-    [Test]
-    public void Part2_Should_Output_Number_Of_Places_Visited_By_Guard_When_He_Has_To_Turn_Right_Twice()
-    {
-        char[][] fakeInput =
-        [
-            "#....".ToArray(),
+            ".#...".ToArray(),
             "....#".ToArray(),
-            "^....".ToArray(),
-            ".....".ToArray(),
-            ".....".ToArray()
-        ];
-        
-        string expectedMessage = $"Number of different visited places: 8";
-        Mock<IPuzzleInputHelper> puzzleInputHelperMock = new Mock<IPuzzleInputHelper>();
-        puzzleInputHelperMock.Setup(p => p.GetInputMatrix("Day06.txt")).Returns(fakeInput);
-        Day06 day06 = new Day06(puzzleInputHelperMock.Object);
-        Mock<TextWriter> tw = new Mock<TextWriter>();
-        Console.SetOut(tw.Object);
-        
-        day06.Part1();
-        
-        tw.Verify(t => t.WriteLine(expectedMessage), Times.Once);
-    }
-    
-    [Test]
-    public void Part2_Should_Output_Number_Of_Places_Visited_By_Guard_When_He_Has_To_Go_Over_His_Path()
-    {
-        char[][] fakeInput =
-        [
-            "#....".ToArray(),
-            "....#".ToArray(),
-            "^....".ToArray(),
+            ".^...".ToArray(),
             "...#.".ToArray(),
             ".....".ToArray()
         ];
         
-        string expectedMessage = $"Number of different visited places: 8";
+        string expectedMessage = $"Number of places to create loop: 1";
         Mock<IPuzzleInputHelper> puzzleInputHelperMock = new Mock<IPuzzleInputHelper>();
         puzzleInputHelperMock.Setup(p => p.GetInputMatrix("Day06.txt")).Returns(fakeInput);
         Day06 day06 = new Day06(puzzleInputHelperMock.Object);
